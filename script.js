e190475eec84f31ee3412dffc600ebff45dbe866
fahimrahman143 <tasfiqurrahman32@gmail.com>
2025-07-02 00:46:07 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+#🧠 script.js:*
+
+```javascript
+document.querySelectorAll('.question').forEach(button => {
+    button.addEventListener('click', () => {
+    const answer = button.nextElementSibling;
+        answer.style.display = answer.style.display === 'block' ? 'none' : 'block';
+    });
+});
+```
